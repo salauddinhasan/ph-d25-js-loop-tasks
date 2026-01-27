a7f6d6f81086840a0404dcc5e396487b36c71074
salauddin <salauddin@hasan.com>
2026-01-27 15:14:46 +0600

    first commit

---
+++ century.js
@@ -0,0 +1,15 @@
+
+
+
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+ 
+
+for (let num = 1; num <= 200; num++) {
+    console.log(num);
+
+
+    if(num === 100) {
+        break;
+    }
+}
