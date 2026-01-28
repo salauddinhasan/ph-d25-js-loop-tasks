8decbfe59e60d53e7f4d081c6638a77243b97dea
salauddin <salauddin@hasan.com>
2026-01-28 12:00:45 +0600

    Practice incremental and decremental for loops

---
+++ decremental-for.js
@@ -0,0 +1,27 @@
+ 
+// incremental loop
+
+for(let i = 0; i <= 10; i++) {
+    console.log(i);
+
+}
+
+
+
+// decremental for loop 
+
+
+for (let n = 10; n >= 0; n--) {
+    console.log(n);
+
+
+}
+
+
+
+
+for (let sum = 20; sum >= 11; sum--) {
+    
+
+    console.log(sum);
+}
