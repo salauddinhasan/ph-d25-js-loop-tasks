87f88f6bb05c95a4e7a45efba71776f6519658a3
salauddin <salauddin@hasan.com>
2026-01-28 12:08:31 +0600

    Loop practice: odd numbers, even numbers, divisible by 3 and 5

---
+++ different.js
@@ -0,0 +1,34 @@
+
+// odd numbers 
+
+for (let i = 0; i < 20; i++) {
+
+    if(i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+for(let n = 0; n < 20; n++) {
+    
+    if(n % 2 !== 0) {
+        console.log(n);
+    }
+}
+
+
+for(let p = 0; p < 30; p+=2) {
+
+     console.log(p)
+}
+
+
+
+// give me the list numbers between 1 to 30 divisible b
+5
+
+for(let a = 1; a <= 30; a++) {
+
+    if(a % 3 === 0 && a % 5 === 0) {
+        console.log(a);
+    }
+}
